f0d0ba834a084e78d3f35f06e7e22ff032a9f5b8
xyhys33 <1073069405@qq.com>
2022-01-06 15:30:29 +0800

    增加了无情帅远

--- src/com/dletc/test/test.java
+++ src/com/dletc/test/test.java
@@ -4,6 +4,7 @@ public class test {
     public static void main(String[] args) {
         System.out.println("无情帅宇");
         System.out.println("dev");
+        System.out.println("无情帅远");
     }
 }
 
